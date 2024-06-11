eb322a2e6b3af543162f41cace97ef247c1cfaac
50022445 <5123134@proton.me>
2024-06-11 12:33:01 -0700

    Added functions

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,10 +1,22 @@
+import { Socket } from "phoenix";
+
 async function redirectUserToChat() {
+    username = sessionStorage.getItem('username')
+     window.socket = new Socket("socket", {
+        params: {
+            username: username
+        }
+    })
+    console.log(window.socket);
+    window.socket.connect()
+
     const uuid = generateChatUuid();
     window.location = `/chat/${uuid}`
 }
 
-function connectToChannel(socket, chatUuid, username) {
-    let channel = socket.channel(`room:${chatUuid}`, {
+function connectToChannel(username) {
+    const uuid = window.location.href.split("/").slice(-1)[0]
+    let channel = window.socket.channel(`room:${uuid}`, {
         username: username
     })
     channel.join()
