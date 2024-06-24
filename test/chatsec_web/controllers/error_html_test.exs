7b2087951a50e6d0f1dffd60a7e35bec4da438cb
Yeno Reus <yeno.reus@bettyblocks.com>
2024-06-24 11:26:04 +0200

    Deleted tests which are not needed anymore

--- test/chatsec_web/controllers/error_html_test.exs
+++ test/chatsec_web/controllers/error_html_test.exs
@@ -1,14 +1,3 @@
 defmodule ChatsecWeb.ErrorHTMLTest do
   use ChatsecWeb.ConnCase, async: true
-
-  # Bring render_to_string/4 for testing custom views
-  import Phoenix.Template
-
-  test "renders 404.html" do
-    assert render_to_string(ChatsecWeb.ErrorHTML, "404", "html", []) == "Not Found"
-  end
-
-  test "renders 500.html" do
-    assert render_to_string(ChatsecWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
-  end
 end
