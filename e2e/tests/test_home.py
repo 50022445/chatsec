2985b22a069caac1249c04b0192f369b5a421672
Yeno Reus <yeno.reus@bettyblocks.com>
2024-07-01 15:55:16 +0200

    updated e2e test to include the random username button

--- e2e/tests/test_home.py
+++ e2e/tests/test_home.py
@@ -24,19 +24,18 @@ def test_opening_page(setup):
     page = setup
     expect(page).to_have_title(re.compile("ChatSec"))
 
-def test_set_username(setup):
+def test_set_username_manual(setup):
     page = setup
     page.click("text='Set username'")
+    page.locator("text='Enter Username'").wait_for(state="visible")
     page.get_by_placeholder("Username").fill("Playwright")
     page.click("text='Submit'")
+    assert page.locator("text='Username set.'").is_visible(), "Failed to enter username manually!"
 
-def test_create_room(setup):
+def test_set_username_random(setup):
     page = setup
     page.click("text='Set username'")
-    page.get_by_placeholder("Username").fill("Playwright")
+    page.locator("text='Enter Username'").wait_for(state="visible")
+    page.click("text='Random'")
     page.click("text='Submit'")
-
-    page.click("text='Start a chat'")
-    
-    # Wait for the URL to match the regex pattern for UUID4
-    page.wait_for_url(url=re.compile(r"/chat/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"))
+    assert page.locator("text='Username set.'").is_visible(), "Failed to use random username!"
